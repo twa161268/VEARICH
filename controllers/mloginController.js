a99f72833a0f50e26210aa2e489689401c75c6d3
Widianto Adi <twa161268@gmail.com>
2026-08-24 14:38:34 +0700

    Update manajemen login

--- controllers/mloginController.js
+++ controllers/mloginController.js
@@ -1,6 +1,6 @@
 // controllers/karyawanController.js
-const db = require("../db");
-const bcrypt = require("bcrypt");
+const db = require('../db');
+const bcrypt = require('bcrypt');
 
 exports.getByIdk = async (req, res) => {
   try {
@@ -20,124 +20,114 @@ exports.getByIdk = async (req, res) => {
 };
 
 exports.loadDatabyidk = async (req, res) => {
-    
-    const idk = req.params.idk; //params idk disini maksudnya ini dari route yg ada :idk nya
+  const idk = req.params.idk; //params idk disini maksudnya ini dari route yg ada :idk nya
 
-    const sql = `
+  const sql = `
     SELECT *
     FROM users
-    WHERE id = $1
+    WHERE user_id = $1
 `;
 
-    try {
-        const rows = await db.query(sql, [idk]);
+  try {
+    const rows = await db.query(sql, [idk]);
 
-        res.json({
-            success: true,
-            data: rows[0] || null
-        });
-
-    } catch (err) {
-        console.error(err);
-        res.json({ success: false });
-    }
-
+    res.json({
+      success: true,
+      data: rows[0] || null,
+    });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
 };
 
 exports.loadDatak = async (req, res) => {
-
-    const sql = `
-    SELECT  id, username, password, nama, role, is_active, created_at 
+  const sql = `
+    SELECT  user_id, username, stkid, password_hash, full_name, role, is_active, created_at,created_by, updated_at, updated_by
     FROM users
     ORDER BY username
     `;
-    try {
-        //const rows = await db.query(sql);
-        const rows = await db.query(sql);
-        res.json({ success: true, data: rows });
-    } catch (err) {
-        res.json({ success: false });
-    }
+  try {
+    //const rows = await db.query(sql);
+    const rows = await db.query(sql);
+    res.json({ success: true, data: rows });
+  } catch (err) {
+    res.json({ success: false });
+  }
 };
-
 
 // READ
 exports.getAll = async (req, res) => {
-    const result = await db.query("SELECT * FROM USERS ORDER BY username");
-    res.render("mlogin", { data: result.rows });
+  const result = await db.query('SELECT * FROM USERS ORDER BY username');
+  res.render('mlogin', { data: result.rows });
 };
 
 // CREATE
 exports.create = async (req, res) => {
-
-    try{
-    const { usernm, userpassword, usernama,userrole,useractive } = req.body;
-    console.log(usernm, userpassword, usernama,userrole,useractive);
-    const cek = await db.query(
-        "SELECT 1 FROM users WHERE username = $1",
-        [usernm]
-    );
+  try {
+    const { usernm, userpassword, stkid, namafull, userrole, useractive } =
+      req.body;
+    console.log(usernm, userpassword, namafull, userrole, useractive);
+    const cek = await db.query('SELECT 1 FROM users WHERE username = $1', [
+      usernm,
+    ]);
 
     if (cek.length > 0) {
-        return res.json({ success: false, msg: "Username sudah ada!" });
+      return res.json({ success: false, msg: 'Username sudah ada!' });
     }
 
     if (!usernm || !userpassword) {
-        return res.json({ success: false, msg: "Username & password wajib!" });
+      return res.json({ success: false, msg: 'Username & password wajib!' });
     }
 
     const userpass = await bcrypt.hash(userpassword, 10);
-    console.log(usernm, userpass, usernama,userrole,useractive);
+    console.log(usernm, userpass, namafull, userrole, useractive);
     await db.query(
-        "INSERT INTO users (username, password, nama, role,is_active, created_at) VALUES ($1,$2,$3,$4,$5,now())",
-        [usernm, userpass, usernama,userrole,useractive]
+      'INSERT INTO users (username, password_hash, full_name, stkid, role,is_active, created_at, created_by) VALUES ($1,$2,$3,$4,$5,$6,now(),$7)',
+      [
+        usernm,
+        userpass,
+        namafull,
+        stkid,
+        userrole,
+        useractive,
+        req.session.username,
+      ]
     );
 
     //res.sendStatus(200);
-    res.json({ success: true, msg: "Insert berhasil!" });
-
-    }catch (e) {
-        console.error(e);
-        res.json({ success: false, msg: "Insert error!" });
-    } 
-}
-
+    res.json({ success: true, msg: 'Insert berhasil!' });
+  } catch (e) {
+    console.error(e);
+    res.json({ success: false, msg: 'Insert error!' });
+  }
+};
 
 // UPDATE
 exports.update = async (req, res) => {
-
-    try {
+  try {
     const d = req.body;
 
-
     if (!d.userid) {
-      return res.json({ success: false, msg: "ID wajib ada!" });
+      return res.json({ success: false, msg: 'ID wajib ada!' });
     }
 
     // ❗ penting: jangan ikutkan field WHERE ke data update
-   
- 
-   const dataUpdate = {
-        username: d.usernm,
-        nama: d.usernama,
-        role: d.userrole,
-        is_active: d.useractive
+
+    const dataUpdate = {
+      username: d.usernm,
+      full_name: d.namafull,
+      stkid: d.stkid,
+      role: d.userrole,
+      is_active: d.useractive,
     };
 
+    await updateByKeys('users', dataUpdate, { user_id: d.userid }, db);
 
-    await updateByKeys(
-      "users",
-      dataUpdate,
-      { id: d.userid },
-      db
-    );
-
-    res.json({ success: true, msg: "Update berhasil!" });
-
+    res.json({ success: true, msg: 'Update berhasil!' });
   } catch (e) {
-
     console.error(e);
-    res.json({ success: false, msg: "Server error!" });
+    res.json({ success: false, msg: 'Server error!' });
   }
 };
 
@@ -145,17 +135,14 @@ const updateByKeys = async (table, data, where, db) => {
   // ambil field dari data
   const fields = Object.keys(data); //ambil nama fieldnya aja
 
-    // bikin SET clause
-  const setClause = fields
-    .map((f, i) => `${f}=$${i + 1}`)
-    .join(", ");
+  // bikin SET clause
+  const setClause = fields.map((f, i) => `${f}=$${i + 1}`).join(', ');
 
   // bikin WHERE clause
   const whereKeys = Object.keys(where);
   const whereClause = whereKeys
     .map((f, i) => `${f}=$${fields.length + i + 1}`)
-    .join(" AND ");
-   
+    .join(' AND ');
 
   // gabung jadi SQL
   const sql = `
@@ -166,10 +153,10 @@ const updateByKeys = async (table, data, where, db) => {
 
   console.log(sql);
   // values untuk SET
-  const values = fields.map(f => data[f] ?? null);
+  const values = fields.map((f) => data[f] ?? null);
 
   // values untuk WHERE
-  const whereValues = whereKeys.map(f => where[f]);
+  const whereValues = whereKeys.map((f) => where[f]);
 
   const finalValues = [...values, ...whereValues];
 
@@ -180,47 +167,41 @@ const updateByKeys = async (table, data, where, db) => {
   return db.query(sql, finalValues);
 };
 
+exports.remove = async (req, res) => {
+  try {
+    const items = req.body.items;
+    if (!items || items.length === 0) {
+      return res.json({ success: false, msg: 'Tidak ada data!' });
+    }
 
+    let successCount = 0;
+    let failCount = 0;
 
+    for (let row of items) {
+      console.log('Formatted idk for deletion:', row.user_id); // Debug: pastikan format periode benar
 
-exports.remove= async (req, res) => {
-    try {
-        const items = req.body.items;
-        if (!items || items.length === 0) {
-            return res.json({ success: false, msg: "Tidak ada data!" });
-        }
-
-        let successCount = 0;
-        let failCount = 0;
-
-        for (let row of items) {
-
-          console.log("Formatted idk for deletion:", row.id); // Debug: pastikan format periode benar
-
-            // di zperiode tidak dikasih tanda kutip
-            const sql = `
+      // di zperiode tidak dikasih tanda kutip
+      const sql = `
                 DELETE FROM users
-                WHERE id=$1
+                WHERE user_id=$1
             `;
 
-            try {
-                await db.query(sql, [row.id]);
-                successCount++;
-            } catch (err) {
-                console.log("Delete error:", err);
-                failCount++;
-            }
-
-        }
-
-        return res.json({
-            success: true,
-            deleted: successCount,
-            failed: failCount
-        });
-
-    } catch (err) {
-        console.error(err);
-        res.json({ success: false, msg: "Server error!" });
+      try {
+        await db.query(sql, [row.user_id]);
+        successCount++;
+      } catch (err) {
+        console.log('Delete error:', err);
+        failCount++;
+      }
     }
+
+    return res.json({
+      success: true,
+      deleted: successCount,
+      failed: failCount,
+    });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, msg: 'Server error!' });
+  }
 };
